2c87166fb1349ef3da52af7b728d6be24dd9c9b8
Francis McGregor-Macdonald <mcgregf@amazon.com>
2021-11-16 22:36:15 +1300

    Set up with cdk v2

--- batch/infrastructure.py
+++ batch/infrastructure.py
@@ -1,26 +1,92 @@
 # import modules
+import pathlib
+from typing_extensions import runtime
+from aws_cdk.aws_logs import RetentionDays
+from constructs import Construct
 from aws_cdk import (
-    core as cdk,
+    Duration,
     aws_ec2 as ec2,
+    aws_ecr_assets as ecr_assets,
     aws_emr as emr,
+    aws_events as events,
+    aws_events_targets as targets,
     aws_glue as glue,
     aws_iam as iam,
     aws_lakeformation as lf,
+    aws_lambda as _lambda,
     aws_s3 as s3,
+    Aws,
+    RemovalPolicy,
+    Stack,
 )
+from aws_cdk.aws_lambda import Runtime
+
+from pathlib import Path
 
 
-class GlueStack(cdk.Stack):
+class Glue(Stack):
     """create crawlers for the s3 buckets"""
 
     def __init__(
         self,
-        scope: cdk.Construct,
+        scope: Construct,
         id: str,
-        constants: dict = None,
-        **kwargs,
-    ) -> None:
-        super().__init__(scope, id, **kwargs)
+        *,
+        S3_BUCKET_RAW_ARN: str,
+        S3_BUCKET_RAW_NAME: str,
+        GLUE_DB_PREFIX: str,
+        OPENLINEAGE_API: str,
+        OPENLINEAGE_NAMESPACE: str,
+        VPC: ec2.Vpc,
+    ):
+        super().__init__(scope, id)
+
+        # to send lineage data to openlineage
+        tablelineage_lambda = _lambda.DockerImageFunction(
+            self,
+            "tablelineage_lambda",
+            code=_lambda.DockerImageCode.from_image_asset(
+                str(Path(__file__).parent.joinpath("runtime/tablelineage"))
+            ),
+            description="Send events to openlineage",
+            environment={
+                "OPENLINEAGE_API": OPENLINEAGE_API,
+                "OPENLINEAGE_NAMESPACE": OPENLINEAGE_NAMESPACE,
+            },
+            log_retention=RetentionDays.ONE_WEEK,
+            timeout=Duration.seconds(30),
+            vpc=VPC,
+        )
+        # add lambda permissions
+        tablelineage_lambda_policy = iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            actions=["glue:GetTable", "glue:GetTableVersion"],
+            resources=["*"],
+        )
+        # add the role permissions
+        tablelineage_lambda.add_to_role_policy(statement=tablelineage_lambda_policy)
+
+        # event rule to send to tablelineage_lambda
+        tableevents = events.Rule(
+            self,
+            "tablevents",
+            description="Glue table updates to openlineage",
+            targets=[targets.LambdaFunction(tablelineage_lambda)],
+            event_pattern={
+                "source": ["aws.glue"],
+                "detail_type": ["Glue Data Catalog Table State Change"],
+            },
+        )
+
+        # glue database for the tables
+        glue_db_name = GLUE_DB_PREFIX
+        database = glue.CfnDatabase(
+            self,
+            "database",
+            catalog_id=Aws.ACCOUNT_ID,
+            database_input={"Name": glue_db_name},
+        )
+        database.apply_removal_policy(policy=RemovalPolicy.DESTROY)
 
         # glue crawler role
         crawler_role = iam.Role(
@@ -30,17 +96,17 @@ class GlueStack(cdk.Stack):
             inline_policies=[
                 iam.PolicyDocument(
                     statements=[
-                        iam.PolicyStatement(
-                            effect=iam.Effect.ALLOW,
-                            actions=["lakeformation:GetDataAccess"],
-                            resources=["*"],
-                        ),
+                        # iam.PolicyStatement(
+                        #    effect=iam.Effect.ALLOW,
+                        #    actions=["lakeformation:GetDataAccess"],
+                        #    resources=["*"],
+                        # ),
                         iam.PolicyStatement(
                             effect=iam.Effect.ALLOW,
                             actions=["s3:*"],
                             resources=[
-                                constants["s3_bucket_raw"].bucket_arn,
-                                f"{constants['s3_bucket_raw'].bucket_arn}/*",
+                                S3_BUCKET_RAW_ARN,
+                                f"{S3_BUCKET_RAW_ARN}/*",
                             ],
                         ),
                     ]
@@ -54,65 +120,61 @@ class GlueStack(cdk.Stack):
         )
 
         # permisisons if lf
-        if constants["PERMISSIONS"] == "Lake Formation":
-            # lf database permissions for the crawler role
-            lf.CfnPermissions(
-                self,
-                "crawler_role_db_permissions",
-                data_lake_principal=lf.CfnPermissions.DataLakePrincipalProperty(
-                    data_lake_principal_identifier=crawler_role.role_arn
-                ),
-                resource=lf.CfnPermissions.ResourceProperty(
-                    database_resource=lf.CfnPermissions.DatabaseResourceProperty(
-                        name=constants["dl_db_raw"].database_name
-                    )
-                ),
-                permissions=["ALTER", "CREATE_TABLE", "DROP"],
-            )
-
-            # lf location permissions for the crawler role
-            lf.CfnPermissions(
-                self,
-                "crawler_role_loc_permissions",
-                data_lake_principal=lf.CfnPermissions.DataLakePrincipalProperty(
-                    data_lake_principal_identifier=crawler_role.role_arn
-                ),
-                resource=lf.CfnPermissions.ResourceProperty(
-                    data_location_resource=lf.CfnPermissions.DataLocationResourceProperty(
-                        s3_resource=constants["s3_bucket_raw"].bucket_arn
-                    )
-                ),
-                permissions=["DATA_LOCATION_ACCESS"],
-            )
-
+        # if constants["PERMISSIONS"] == "Lake Formation":
+        #     # lf database permissions for the crawler role
+        #    lf.CfnPermissions(
+        #       self,
+        #      "crawler_role_db_permissions",
+        #         data_lake_principal=lf.CfnPermissions.DataLakePrincipalProperty(
+        #            data_lake_principal_identifier=crawler_role.role_arn
+        #        ),
+        #        resource=lf.CfnPermissions.ResourceProperty(
+        #            database_resource=lf.CfnPermissions.DatabaseResourceProperty(
+        #                name=constants["dl_db_raw"].database_name
+        #            )
+        #        ),
+        #        permissions=["ALTER", "CREATE_TABLE", "DROP"],
+        #    )
+        #
+        #            # lf location permissions for the crawler role
+        #            lf.CfnPermissions(
+        #                self,
+        #                "crawler_role_loc_permissions",
+        #                data_lake_principal=lf.CfnPermissions.DataLakePrincipalProperty(
+        #                    data_lake_principal_identifier=crawler_role.role_arn
+        #                ),
+        #                resource=lf.CfnPermissions.ResourceProperty(
+        #                    data_location_resource=lf.CfnPermissions.DataLocationResourceProperty(
+        #                        s3_resource=constants["s3_bucket_raw"].bucket_arn
+        #                    )
+        #                ),
+        #                permissions=["DATA_LOCATION_ACCESS"],
+        #            )
+        #
         # the raw bucket crawler
         crawler_raw = glue.CfnCrawler(
             self,
             "crawler_raw",
             targets=glue.CfnCrawler.TargetsProperty(
-                s3_targets=[
-                    glue.CfnCrawler.S3TargetProperty(
-                        path=constants["s3_bucket_raw"].bucket_name
-                    )
-                ],
+                s3_targets=[glue.CfnCrawler.S3TargetProperty(path=S3_BUCKET_RAW_NAME)],
             ),
             # classifiers=[customer_classifier.csv_classifier.name],
-            database_name=constants["dl_db_raw"].database_name,
+            database_name=glue_db_name,
             role=crawler_role.role_name,
         )
 
         # create glue job for raw to processed
-        glue_job_processed = glue.CfnJob(
-            self, "glue_job_processed"  # default_arguments={"--conf": "", "--conf": ""}
-        )
+        # glue_job_processed = glue.CfnJob(
+        #    self, "glue_job_processed"  # default_arguments={"--conf": "", "--conf": ""}
+        # )
 
 
-class EMR(cdk.Stack):
+class EMR(Stack):
     """ """
 
     def __init__(
         self,
-        scope: cdk.Construct,
+        scope: Construct,
         id: str,
         *,
         VPC: ec2.Vpc,
@@ -132,7 +194,7 @@ class EMR(cdk.Stack):
             encryption=s3.BucketEncryption.S3_MANAGED,
             public_read_access=False,
             block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
-            removal_policy=cdk.RemovalPolicy.DESTROY,
+            removal_policy=RemovalPolicy.DESTROY,
             auto_delete_objects=True,
         )
 
@@ -226,7 +288,7 @@ class EMR(cdk.Stack):
                     configuration_properties={"maximizeResourceAllocation": "true"},
                 ),
             ],
-            log_uri=f"s3://{s3_bucket_emr.bucket_name}/{cdk.Aws.REGION}/elasticmapreduce/",
+            log_uri=f"s3://{s3_bucket_emr.bucket_name}/{Aws.REGION}/elasticmapreduce/",
             release_label=EMR_RELEASE_LABEL,
             visible_to_all_users=True,
             # the job to be done
