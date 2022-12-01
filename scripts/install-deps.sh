6ebcbbb70897882df9ad9eb70a3d3ecae0fb33e1
Stephen Said <saidst@amazon.de>
2022-12-01 12:59:34 +0100

    tweak install script

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o verbose
 
 # Install ckd project dependencies
-.env/bin/python -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # zip the plugins (if required)
 cd ./orchestration/runtime/mwaa/plugins; zip -r ../plugins.zip ./; cd ../../../../
